46d48bae49ff0a1ead977f7d0e276dfee081dee2
mtianxiang <maotianxiang@bao.ac.cn>
2016-09-19 16:29:14 +0800

    1026-09-19

--- src/creatALLid.py
+++ src/creatALLid.py
@@ -9,13 +9,13 @@ class CreatAllId(UCAS):
         Year = ['2012', '2013', '2014', '2015','2016']
         Dw = [str(i) for i in np.loadtxt('../data/danwei.txt', dtype=np.int32)]
         Xk = ['%02d' % i for i in np.arange(1, 51)]
-        xxx = ['%03d' % i for i in np.arange(1, 30)]
+        xxx = ['%03d' % i for i in np.arange(1, 3)]
         userID = [
             i +
             j +
             k +
             h +
-            g for i in Year for j in Sb for k in Dw for h in Xk for g in xxx]
+            g for j in Sb for i in Year for g in xxx for h in Xk for k in Dw]
         userID = np.array(userID)
         np.save('../data/ALLid.npy',userID)
         print 'creat ALLid ......'
